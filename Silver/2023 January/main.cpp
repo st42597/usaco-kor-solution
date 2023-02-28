ed0c4f6b5e6ac786fd994a19f2e06f2da7ba3490
SeongHyeon Cho <67233988+st42597@users.noreply.github.com>
2023-02-28 17:32:45 +0900

    add problem

---
+++ Silver/2023 January/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[100003];
+bool check[100003];
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int N; cin >> N;
+    int cnt = 0;
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+        cnt += arr[i];
+    }
+    check[N] = true;
+    int pos = 0, dir = 1;
+    while(cnt--){
+        if(pos == 0) dir = 1;
+        if(dir == 1){
+            if(arr[pos] > 0){
+                arr[pos++]--;
+                cout << 'R';
+            }else{
+                if(--arr[--pos] == 0){
+                    check[pos] = check[pos + 1];
+                }
+                dir = -1;
+                cout << 'L';
+            }
+        }else{
+            if(arr[pos - 1] > 1){
+                arr[--pos]--;
+                cout << 'L';
+            }else if(arr[pos - 1] == 1 && check[pos]){
+                arr[--pos]--;
+                check[pos] = check[pos + 1];
+                cout << 'L';
+            }else{
+                arr[pos++]--;
+                cout << 'R';
+                dir = 1;
+            }
+        }
+    }
+
+    return 0;
+}
